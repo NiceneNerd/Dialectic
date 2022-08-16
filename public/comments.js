fb89658ce54f54fdab5cda8d26dce7f0bf2718ad
Caleb Smith <c.smith@tuta.io>
2022-08-16 07:22:03 -0500

    Add avatars and upvotes

--- public/comments.js
+++ public/comments.js
@@ -1,20 +1,38 @@
 const comments = {
     async load() {
-        const response = await fetch('/api/comments');
+        const response = await fetch("/api/comments");
         const data = await response.json();
-        let commentList = document.getElementById('comment-list');
-        commentList.innerHTML = '';
-        const template = document.getElementById('comment-template');
+        let commentList = document.getElementById("comment-list");
+        commentList.innerHTML = "";
+        const template = document.getElementById("comment-template");
         if (data.length > 0) {
             data.forEach(comment => {
                 let clone = template.content.cloneNode(true);
-                clone.querySelector('.comment-author-name').textContent = comment.name;
-                clone.querySelector('.comment-author-date').textContent = new Date(comment.date_posted).toLocaleString();
-                clone.querySelector('.comment-body').textContent = comment.body;
+                clone.querySelector(".avatar").src =
+                    "/images/" + comment.name + ".jpg";
+                clone.querySelector(".comment-author-name").textContent =
+                    comment.name;
+                clone.querySelector(".comment-author-date").textContent =
+                    new Date(comment.date_posted).toLocaleString();
+                clone.querySelector(".comment-body").textContent = comment.body;
+                let upvote_button = clone.querySelector(
+                    ".comment-action-upvote"
+                );
+                upvote_button.textContent += ` (${comment.upvotes})`;
+                upvote_button.dataset.commentId = comment.id;
                 commentList.appendChild(clone);
             });
         } else {
-            commentList.innerHTML = '<p>No comments yet</p>';
+            commentList.innerHTML = "<p>No comments yet</p>";
         }
+    },
+
+    async upvote(commentId) {
+        console.log(commentId);
+        const response = await fetch(`/api/comments/upvote/${commentId}`, {
+            method: "PUT"
+        });
+        const data = response.status;
+        console.log(data);
     }
 };
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ use diesel::prelude::*;
 use models::NewComment;
 mod models;
 mod schema;
-use self::{models::Comment, schema::comments::dsl::*};
+use self::{
+    models::Comment,
+    schema::comments::dsl::{comments, upvotes},
+};
 use rocket::{request::Form, *};
 use rocket_contrib::{
     database, databases::diesel::MysqlConnection, json::Json, serve::StaticFiles,
@@ -24,17 +27,26 @@ fn get_comments(conn: CommentDbConn) -> Json<Vec<Comment>> {
 }
 
 #[post("/comments", data = "<comment>")]
-fn new_comment(comment: Form<NewComment>, conn: CommentDbConn) {
+fn new_comment(comment: Form<NewComment>, conn: CommentDbConn) -> response::Redirect {
     diesel::insert_into(comments)
         .values(&comment.into_inner())
         .execute(&*conn)
         .expect("Error saving new comment");
+    response::Redirect::to("/")
+}
+
+#[put("/comments/upvote/<id>")]
+fn upvote_comment(id: u64, conn: CommentDbConn) {
+    let rows = diesel::update(comments.find(id))
+        .set(upvotes.eq(upvotes + 1))
+        .execute(&*conn)
+        .expect("Error updating comment");
 }
 
 fn main() {
     rocket::ignite()
         .attach(CommentDbConn::fairing())
-        .mount("/api", routes![get_comments, new_comment])
+        .mount("/api", routes![get_comments, new_comment, upvote_comment])
         .mount("/", StaticFiles::from("./public"))
         .launch();
 }
